be35b7cc10468264ce697a406ef9936dda73a659
rymo90 <red.01yassin@gmail.com>
2019-08-28 18:18:56 +0400

    first commit

---
+++ src/components/Carts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Carts extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Carts</h3>
+            </div>
+        )
+    }
+}
